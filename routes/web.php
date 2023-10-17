d27c027d15c547d55b687af48c716b9287b5d432
Bruno Bastos <brunomauro@gmail.com>
2023-10-17 14:17:03 -0300

    wip: rotas

--- routes/web.php
+++ routes/web.php
@@ -6,41 +6,37 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-use App\Http\Controllers\BrowsershotContoller;
-use App\Http\Controllers\LetterheadController;
-use App\Http\Controllers\PhpwordController;
-use App\Http\Controllers\SnappyController;
-
 //salvar os dados do vuejs na session para testes rápidos
-Route::middleware('web')->post('/save-configs', function (Request $request){
+Route::middleware('web')->post('/save-configs', function(Request $request) {
     session()->remove('configs');
     $request->session()->put('configs', $request->all());
     return response()->json(['status' => 'success']);
 })->name('save-configs');
 
 
-Route::get('snappy', SnappyController::class);
-Route::get('browsershot', BrowsershotContoller::class);
-Route::get('phpword', PhpwordController::class);
-Route::get('letterhead', LetterheadController::class);
+Route::get('teste', \App\Http\Controllers\TesteController::class);
+Route::get('letterhead', \App\Http\Controllers\LetterheadController::class);
+Route::get('snappy', \App\Http\Controllers\SnappyController::class);
+Route::get('browsershot', \App\Http\Controllers\BrowsershotContoller::class);
+Route::get('phpword', \App\Http\Controllers\PhpwordController::class);
 
-Route::get('/', function () {
+Route::get('/', function() {
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
+        'canLogin'       => Route::has('login'),
+        'canRegister'    => Route::has('register'),
         'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+        'phpVersion'     => PHP_VERSION,
     ]);
 });
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard', function() {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function() {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
