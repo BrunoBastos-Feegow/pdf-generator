b69156245da3174b157e55279392d8269b41d5d3
Bruno Bastos <brunomauro@gmail.com>
2023-10-18 14:40:25 -0300

    wip: load and save letterhead

--- app/Http/Controllers/LetterheadController.php
+++ app/Http/Controllers/LetterheadController.php
@@ -2,13 +2,26 @@
 //app/Http/Controllers/LetterheadController.php
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LetterheadRequest;
+use App\Services\LetterheadService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class LetterheadController extends Controller
 {
-    public function __invoke()
+    public function __construct(
+        protected LetterheadService $letterheadService
+    ) {}
+
+    public function index(int $id = null)
     {
-        return Inertia::render('Letterhead');
+        $letterhead = $this->letterheadService->getLetterhead($id);
+        return Inertia::render('Letterhead', compact('letterhead'));
+    }
+
+    public function save(LetterheadRequest $request)
+    {
+        $letterhead = $this->letterheadService->saveLetterhead($request->all());
+        return redirect()->route('letterhead', ['id' => $letterhead->id]);
     }
 }
