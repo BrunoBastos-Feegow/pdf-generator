1017d2238d0ae0a22aa6676252218bb89cd24c0f
Bruno Bastos <brunomauro@gmail.com>
2023-11-29 18:20:44 -0300

    feat: [wip] controller with annotation

--- app/Http/Controllers/SnappyController.php
+++ app/Http/Controllers/SnappyController.php
@@ -5,13 +5,47 @@ namespace App\Http\Controllers;
 use App\Services\SnappyService;
 use Illuminate\Http\Request;
 
+/**
+ * Class SnappyController
+ * @package App\Http\Controllers
+ *
+ * @group PDF generation
+ */
 class SnappyController extends Controller
 {
     public function __construct(
         private SnappyService $snappyService
     ) {}
 
-    public function basicHtmlToPdf(Request $request)
+    /**
+     * Basic PDF generation
+     *
+     * Generate PDFs from HTML
+     *
+     * @bodyParam html string required The HTML to be converted to PDF. Example: <html><body><h1>Teste</h1></body></html>
+     *
+     * @response 200 scenario="Success" {
+     * "Content-Type": "application/pdf",
+     * "Content-Disposition": "inline; filename="2021-09-28 14:09:00.pdf"",
+     * "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate"
+     * }
+     *
+     * @response 422 scenario="Validation error"
+     *  {
+     *      "message": "The given data was invalid.",
+     *      "errors": {
+     *          "html": [
+     *              "O campo html é obrigatório."
+     *          ]
+     *      }
+     *  }
+     *
+     * @response 500 scenario="Internal server error"
+     *  {
+     *      "message": "Internal server error"
+     *  }
+     */
+    public function basic(Request $request)
     {
         $request->validate([
             'html' => 'required',
@@ -19,34 +53,92 @@ class SnappyController extends Controller
             'html.required' => 'O campo html é obrigatório.',
         ]);
 
-        $pdf = $this->snappyService->basicHtmlToPdf($request->html);
-        return response($pdf, 200, [
+        $snappy = app()->make('snappy.pdf');
+        $html = $request->html;
+        $body   = view('snappy.raw-html', compact('html'))->render();
+        return response($snappy->getOutputFromHtml($body), 200, [
             'Content-Type'        => 'application/pdf',
-            'Content-Disposition' => 'inline; filename="' . now() . '.pdf"',
-            'Cache-Control'       => 'no-cache, no-store, max-age=0, must-revalidate',
+            'Content-Disposition' => 'inline; filename = "' . now() . '.pdf"',
+            'Cache-Control'       => 'no-cache, no-store, max-age = 0, must-revalidate',
         ]);
+
+//        $pdf = $this->snappyService->basicHtmlToPdf($request->html);
+//        return response($pdf, 200, [
+//            'Content-Type'        => 'application/pdf',
+//            'Content-Disposition' => 'inline; filename="' . now() . '.pdf"',
+//            'Cache-Control'       => 'no-cache, no-store, max-age=0, must-revalidate',
+//        ]);
     }
 
-    public function advancedHtmlToPdf(Request $request)
+    /**
+     * Advanced PDF generation
+     *
+     * Generate PDFs from HTML with header and footer repeating them in every page
+     *
+     * @bodyParam settings object required The settings for the PDF generation. Example: {"useHeader": true, "useFooter": true, "paperSize": "A4", "orientation": "portrait"}
+     * @bodyParam settings.useHeader boolean  Whether to use header or not. Example: true
+     * @bodyParam settings.useFooter boolean  Whether to use footer or not. Example: true
+     * @bodyParam settings.paperSize string  The paper size. Example: A4
+     * @bodyParam settings.orientation string  The orientation. Example: portrait
+     *
+     * @bodyParam header string required The HTML for the header. Example: <html><body><h1>Header</h1></body></html>
+     * @bodyParam body string required The HTML for the body. Example: <html><body><h1>Body</h1></body></html>
+     * @bodyParam footer string required The HTML for the footer. Example: <html><body><h1>Footer</h1></body></html>
+     *
+     * @response 200 scenario="Success" {
+     * "Content-Type": "application/pdf",
+     * "Content-Disposition": "inline; filename="2021-09-28 14:09:00.pdf"",
+     * "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate"
+     * }
+     *
+     * @response 422 scenario="Validation error"
+     *  {
+     *      "message": "The given data was invalid.",
+     *      "errors": {
+     *          "settings": [
+     *              "O campo settings é obrigatório."
+     *          ],
+     *          "header": [
+     *              "O campo header é obrigatório."
+     *          ],
+     *          "body": [
+     *              "O campo body é obrigatório."
+     *          ],
+     *          "footer": [
+     *              "O campo footer é obrigatório."
+     *          ]
+     *     }
+     * }
+     *
+     * @response 500 scenario="Internal server error"
+     * {
+     *     "message": "Internal server error"
+     * }
+     *
+     */
+    public function advanced(Request $request)
     {
         $request->validate([
-            'letterhead' => 'required|array',
-            'body' => 'required',
-            'default' => 'required',
-            'specialControl' => 'required',
+            'settings' => 'required|array',
+            'header'   => 'required',
+            'body'     => 'required',
+            'footer'   => 'required',
         ], [
-            'letterhead.required' => 'O campo letterhead é obrigatório.',
-            'body.required' => 'O campo body é obrigatório.',
-            'default.required' => 'O campo default é obrigatório.',
-            'specialControl.required' => 'O campo specialControl é obrigatório.',
+            'settings.required' => 'O campo settings é obrigatório.',
+            'settings.array'    => 'O campo settings deve ser um array.',
+            'header.required'   => 'O campo header é obrigatório.',
+            'body.required'     => 'O campo body é obrigatório.',
+            'footer.required'   => 'O campo footer é obrigatório.',
         ]);
 
         $pdf = $this->snappyService->advancedHtmlToPdf(
             $request->letterhead,
             $request->body,
             $request->default,
-            $request->specialControl
+            $request->specialControl,
+            $request->isSpecialControl ?? false
         );
+
         return response($pdf, 200, [
             'Content-Type'        => 'application/pdf',
             'Content-Disposition' => 'inline; filename="' . now() . '.pdf"',
@@ -54,10 +146,10 @@ class SnappyController extends Controller
         ]);
     }
 
-    public function testeFixedContent()
+    public function testFixedContent()
     {
         //define a conversion rate from mm to pixels
-        define('PIXELS_PER_MM', (96 / 25.4));
+//        define('PIXELS_PER_MM', (96 / 25.4));
         $configs      = session()->get('configs');
         $topMargin    = ($configs['topMargin'] ?? 20) / PIXELS_PER_MM;
         $bottomMargin = ($configs['bottomMargin'] ?? 20) / PIXELS_PER_MM;
@@ -108,8 +200,8 @@ class SnappyController extends Controller
 
         return response($snappy->getOutputFromHtml($body), 200, [
             'Content-Type'        => 'application/pdf',
-            'Content-Disposition' => 'inline; filename="' . now() . '.pdf"',
-            'Cache-Control'       => 'no-cache, no-store, max-age=0, must-revalidate',
+            'Content-Disposition' => 'inline; filename = "' . now() . '.pdf"',
+            'Cache-Control'       => 'no-cache, no-store, max-age = 0, must-revalidate',
         ]);
     }
 }
