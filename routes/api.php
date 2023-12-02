41a0069adc56a50ae372b24836461b9fbf26273f
Bruno Bastos <brunomauro@gmail.com>
2023-12-02 10:36:29 -0300

    feat: route for dynamic reports

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('test-fixed-content', [SnappyController::class, 'testFixedContent'])->name('snappy.test-fixed-content');
 
 Route::post('basic', [SnappyController::class, 'basic'])->name('snappy.basic');
-Route::post('advanced', [SnappyController::class, 'advanced'])->name('snappy.advanced');
+Route::post('dynamic-report', [SnappyController::class, 'dynamicReport'])->name('snappy.dynamic-report');
 
